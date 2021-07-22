f757b1304d1b595ac12823f76b0f1314a103a362
mahendranathkamisetty <34596078+mahendranathkamisetty@users.noreply.github.com>
2021-07-22 23:29:03 +0530

    Checkpoint 12

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const API_URL = "https://tripadvisor1.p.rapidapi.com/";
+const tripAdvisorHost = "tripadvisor1.p.rapidapi.com";
+const tripAdvisorKey = "6049913967mshcedadace2695cddp117ca1jsn6b7cf16d6ad6";
+
 function view(){
     let viewEle1 = document.getElementById("viewmore-button");
     if(viewEle1.innerText == "View more"){
